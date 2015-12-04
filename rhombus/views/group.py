f7879ce4644aad8a8c0a9627ede725213980497e
Anto - alrescha <anto@eijkman.go.id>
2015-12-04 20:30:10 +0700

    added stubs for group listing

--- rhombus/views/group.py
+++ rhombus/views/group.py
@@ -1,7 +1,31 @@
 
+from rhombus.views import *
+
+from rhombus.models.user import Group
 
 
+@roles( PUBLIC )
 def index(request):
+    """ list groups """
+
+    dbh = get_dbhandler()
+    groups = Group.query(dbh.session()).order_by( Group.name )
+
+    t = table(class_='table table-condensed table-striped')
+    t.add( thead()[
+            tr()[ th(), th('Group Name'), th('Members') ]
+        ])
+
+    tb = tbody()
+    for grp in groups:
+        tb.add( tr()[ td(), td(grp.name), td(len(grp.users)) ] )
+
+    t.add( tb )
+
+
+
+    return render_to_response('rhombus:templates/group/index.mako',
+                { 'groups': groups }, request=request )
     raise NotImplementedError
 
 
