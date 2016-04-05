a01e8803f0b7ad2478a0225e1a706cfe8c16376d
Hidayat Trimarsanto <anto@eijkman.go.id>
2016-04-05 10:57:37 +0700

    /mgr now asks for login instead of non-authorized error

--- rhombus/views/dashboard.py
+++ rhombus/views/dashboard.py
@@ -7,8 +7,16 @@ from pyramid.renderers import render_to_response
 from rhombus.views import roles
 from rhombus.lib.roles import SYSADM, SYSVIEW
 
-@roles( SYSADM, SYSVIEW )
+
+#@roles( SYSADM, SYSVIEW )
 def index(request):
+
+    if not request.user:
+        import rhombus.views.home
+        return rhombus.views.home.login(request)
+
+    if not request.user.has_roles( SYSADM, SYSVIEW ):
+        return error_page(request, 'You are not authorized to access this page!')
 
     return render_to_response( "rhombus:templates/dashboard/index.mako",
         {},
