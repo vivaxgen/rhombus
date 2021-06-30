33ec5d978a620a79f4871e298f3a620a4986c40c
anto_at_prodesk <anto@eijkman.go.id>
2021-06-30 22:46:48 +0700

    use no_autoflush inside from_dict(); format lastuser field properly in as_dict()

--- rhombus/models/autoupdatemixin.py
+++ rhombus/models/autoupdatemixin.py
@@ -10,15 +10,31 @@ Please read the README.txt of this software.
 
 from sqlalchemy import Column
 from sqlalchemy.inspection import inspect
+from sqlalchemy.orm.session import object_session
 
 
 class AutoUpdateMixIn(object):
 
     # this is for caching field/column lookup
+
+    # plain fields are fields that directly hold values, and automatically
+    # filled by parsing all columns when the get_plain_fields() is called
     __plain_fields__ = None
+
+    # nullable fields are plain fields that can have null value, and
+    # automatically filled when get_nullable_fields() is called, as such
+    # nullable fields are subset of plain fields
     __nullable_fields__ = None
-    __ek_fields__ = None
+
+    # ek fields are proxies of any plain fields that relate to EK key, and
+    # the variable needs to be filled manually
+    __ek_fields__ = []
+
+    # rel fields are all relationship parsed automatically when
+    # get_rel_fields() is called
     __rel_fields__ = None
+
+    # auxiliary fields
     __aux_fields__ = None
 
     __excluded_fields__ = {'id', }
@@ -38,15 +54,18 @@ class AutoUpdateMixIn(object):
     def from_dict(cls, a_dict, dbh):
         """ load and add from a dict """
         obj = cls()
-        obj.update(a_dict)
-        dbh.session().add(obj)
+        with dbh.session().no_autoflush:
+            dbh.session().add(obj)
+            obj.update(a_dict)
+
         return obj
 
-    def as_dict(self):
-        return dict(
-            lastuser=self.lastuser.login,
-            stamp=self.stamp,
-        )
+    def as_dict(self, exclude=None):
+        d = self.create_dict_from_fields(exclude=exclude)
+        # check if lastuser field from stampmixin exists
+        if 'lastuser_id' in d and self.lastuser:
+            d['lastuser'] = self.lastuser.login
+        return d
 
     def update_fields_with_dict(self, a_dict, fields=None, exclude=None):
         fields = fields or self.get_plain_fields()
@@ -84,12 +103,12 @@ class AutoUpdateMixIn(object):
                 setattr(self, f_, dbh.EK.getid(a_dict[f], session))
 
     def create_dict_from_fields(self, fields=None, exclude=None):
-        fields = fields or (self.__plain_fields__ + self.__aux_fields__)
+        fields = fields or (self.get_plain_fields() + self.__ek_fields__)
         d = {}
         for f in fields:
             if exclude and f in exclude:
                 continue
-            d[f] = str(getattr(self, f))
+            d[f] = getattr(self, f)
         return d
 
     @classmethod
@@ -119,3 +138,5 @@ class AutoUpdateMixIn(object):
             rels = inspect(cls).relationships
             cls.__rel_fields__ = list(r.key for r in rels if r.key not in cls.__excluded_fields__)
         return cls.__rel_fields__
+
+# end of file
