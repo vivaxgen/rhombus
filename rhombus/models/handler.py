59704f354f3db84e0d14a440fe108e62eb8fbbe8
anto_at_prodesk <anto@eijkman.go.id>
2021-07-03 17:34:19 +0700

    clean up and reformat code to pep8 style

--- rhombus/models/handler.py
+++ rhombus/models/handler.py
@@ -8,7 +8,10 @@ from sqlalchemy.orm import exc
 
 cinfo = print
 
-## query constructor
+
+#
+# query constructor
+#
 
 class QueryConstructor(object):
 
@@ -19,22 +22,19 @@ class QueryConstructor(object):
         'group_id': user.Group.id,
     }
 
-
     def __init__(self):
         pass
-
 
     def construct_query_from_list(self, a_list):
         exprs = []
         classes = []
         for spec in a_list:
-            spec_exprs, spec_classes = self.construct_query_from_dict( spec )
-            exprs.append( spec_exprs )
-            classes.extend( spec_classes )
+            spec_exprs, spec_classes = self.construct_query_from_dict(spec)
+            exprs.append(spec_exprs)
+            classes.extend(spec_classes)
 
-        classes = set( classes )
+        classes = set(classes)
         return or_(* exprs), classes
-
 
     def construct_query_from_dict(self, a_dict):
 
@@ -44,22 +44,20 @@ class QueryConstructor(object):
         for k, val in a_dict.items():
             f = self.field_specs[k]
             if f.class_ not in classes:
-                classes.append( f.class_ )
+                classes.append(f.class_)
             if isinstance(val, list):
-                exprs.append( f.in_(val) )
+                exprs.append(f.in_(val))
             elif '%' in val:
-                exprs.append( f.ilike(val))
+                exprs.append(f.ilike(val))
             else:
-                exprs.append( f == val )
+                exprs.append(f == val)
 
-        return and_( *exprs ), classes
-
+        return and_(*exprs), classes
 
 
 class DBHandler(object):
 
     # put the objectclass that we need to directly access here
-
 
     EK = ek.EK
     Group = user.Group
@@ -69,7 +67,7 @@ class DBHandler(object):
 
     query_constructor_class = QueryConstructor
 
-    def __init__(self, settings, tag='sqlalchemy.', initial = False):
+    def __init__(self, settings, tag='sqlalchemy.', initial=False):
         """ use settings from configfile, prepare self.engine & self.session """
         cinfo("Connecting to database..")
 
@@ -85,17 +83,15 @@ class DBHandler(object):
 
         if not initial and use_logger == 'true':
             cinfo('data logger is being used!')
-            data_logger = DataLogger()
-            set_datalogger( data_logger )
+            data_logger = core.DataLogger()
+            meta.set_datalogger(data_logger)
 
-            get_clsreg().sync()
+            core.get_clsreg().sync()
 
         self.settings = settings
         self.session = meta.get_dbsession()
-        self.session.configure(bind = self.engine )
+        self.session.configure(bind=self.engine)
         self._query_constructor = None
-
-
 
     def initdb(self, create_table=True, init_data=True, rootpasswd=None):
         """ prepare the database for the first time by initializing it with
@@ -108,9 +104,8 @@ class DBHandler(object):
 
         if init_data:
             from rhombus.models.setup import setup
-            setup( self, rootpasswd )
+            setup(self, rootpasswd)
             cerr('[rhombus] Database has been initialized.')
-
 
     def get_userclass(self, userclass=None):
 
@@ -118,7 +113,7 @@ class DBHandler(object):
             return self.UserClass.query(self.session()).all()
 
         if type(userclass) == list:
-            return [ self.get_userclass(x) for x in userclass ]
+            return [self.get_userclass(x) for x in userclass]
 
         if type(userclass) == int:
             return self.UserClass.get(userclass, self.session())
@@ -127,20 +122,18 @@ class DBHandler(object):
 
         raise RuntimeError('ERR: unknown data type for getting UserClass!')
 
-
     def get_user(self, user=None):
 
         if user is None:
             return self.User.query(self.session()).all()
 
         if type(user) == list:
-            return [ self.get_user(u) for u in user ]
+            return [self.get_user(u) for u in user]
 
         if type(user) == int:
             return self.User.get(user, self.session())
 
         return self.User.search(user, session=self.session())
-
 
     def get_group(self, group=None, user_id=None):
 
@@ -152,61 +145,52 @@ class DBHandler(object):
                 raise NotImplementedError()
 
         if type(group) == list:
-            return [ self.get_group(g) for g in group ]
+            return [self.get_group(g) for g in group]
 
         if type(group) == int:
             return self.Group.get(group, self.session())
 
         return self.Group.search(group, self.session())
 
-
     def get_groups(self):
         """ return all non-system groups """
 
         q = user.Group.query(self.session())
-        q = q.filter( ~user.Group.name.startswith('\_', escape='\\') )
+        q = q.filter(~user.Group.name.startswith('\\_', escape='\\'))
         return q.all()
-
 
     def get_user_by_id(self, id):
         return self.User.get(id, self.session())
 
-
     def get_user_by_email(self, email):
-        q = self.User.query(self.session()).filter( self.User.email.ilike(email) )
+        q = self.User.query(self.session()).filter(self.User.email.ilike(email))
         return q.all()
-
 
     def get_group_by_id(self, id):
         return self.Group.get(id, self.session())
 
-
-    ## EnumeratedKey methods
+    # EnumeratedKey methods
 
     def add_ekey(self, ekey, group=None):
         group_ek = None
         if group:
             group_ek = ek.EK.search(group, dbsession=self.session())
 
-
         if not group_ek and not ekey.startswith('@'):
             raise RuntimeError('ekey must be under a group, or starts with @')
 
-        new_ekey = ek.EK( ekey, '-', parent=group_ek)
+        new_ekey = ek.EK(ekey, '-', parent=group_ek)
         self.session().add(new_ekey)
         return new_ekey
-
 
     def list_ekeys(self, group=None):
         if group:
             return ek.EK.getmembers(group, self.session())
         else:
-            return ek.EK.query(self.session()).filter( ek.EK.key.startswith('@') ).all()
-
+            return ek.EK.query(self.session()).filter(ek.EK.key.startswith('@')).all()
 
     def get_ekey(self, ekey):
-        return self.EK.search(ekey, dbsession = self.session())
-
+        return self.EK.search(ekey, dbsession=self.session())
 
     # Universal query system
 
@@ -214,7 +198,6 @@ class DBHandler(object):
         if self._query_constructor is None:
             self._query_constructor = self.query_constructor_class()
         return self._query_constructor
-
 
     def construct_query(self, object, selector):
         """ return compound query constructed from list & dictionary
@@ -233,17 +216,17 @@ class DBHandler(object):
             filter ( or_( and_(Category.id.in_([1,2]), Collection.id.in_([5])) ) )
         """
 
-        q = self.session().query( object )
+        q = self.session().query(object)
 
         selector = selector or []
         constructor = self.get_query_constructor()
-        filter_expr, filter_classes = constructor.construct_query_from_list( selector )
-        filter_classes = filter_classes - { object }
+        filter_expr, filter_classes = constructor.construct_query_from_list(selector)
+        filter_classes = filter_classes - {object}
 
         for class_ in filter_classes:
             q = q.join(class_)
 
-        q = q.filter( filter_expr )
+        q = q.filter(filter_expr)
         return q
 
     def fetch_query(self, query, fetch, raise_if_empty):
