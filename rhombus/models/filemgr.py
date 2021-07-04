98b7aa7c119e6118509cd4f28f03ad495989ba55
anto_at_prodesk <anto@eijkman.go.id>
2021-07-04 22:07:10 +0700

    reformat to pep8 style

--- rhombus/models/filemgr.py
+++ rhombus/models/filemgr.py
@@ -1,6 +1,10 @@
 
-from .core import *
+from .core import (Base, BaseMixIn, Column, types, ForeignKey, relationship, backref, registered,
+                   NoResultFound)
 from .ek import EK
+from rhombus.lib.utils import get_groupid
+
+import pathlib
 
 
 @registered
@@ -27,7 +31,7 @@ class File(BaseMixIn, Base):
     """ mimetype for this file """
 
     group_id = Column(types.Integer, ForeignKey('groups.id'), nullable=False,
-                default = get_groupid)
+                      default=get_groupid)
     """ group_id for this file, default use user's primarygroup_id """
 
     bindata = Column(types.LargeBinary, nullable=False, server_default='')
@@ -46,61 +50,64 @@ class File(BaseMixIn, Base):
     """
 
     entries = relationship("File",
+                           # cascade deletions
+                           cascade="all",
 
-                        # cascade deletions
-                        cascade="all",
+                           # many to one + adjacency list - remote_side
+                           # is required to reference the 'remote'
+                           # column in the join condition.
+                           backref=backref("parent", remote_side='File.id')
 
-                        # many to one + adjacency list - remote_side
-                        # is required to reference the 'remote'
-                        # column in the join condition.
-                        backref=backref("parent", remote_side='File.id')
-
-                        # entries will be represented as a dictionary
-                        # on the "path" attribute.
-                        #collection_class=attribute_mapped_collection('path'),
-                    )
-
-
+                           # entries will be represented as a dictionary
+                           # on the "path" attribute.
+                           # collection_class=attribute_mapped_collection('path'),
+                           )
 
     @property
     def filename(self):
         if self.type == 'file/folder':
             return ''
-        return self.path.rsplit('/',1)[1]
+        return self.path.rsplit('/', 1)[1]
 
     @filename.setter
     def filename(self, filename):
         if self.type == 'file/folder':
             raise RuntimeError('cannot rename filename within folder object')
-        self.path = self.path.rsplit('/',1)[0] + '/' + filename
-
+        self.path = self.path.rsplit('/', 1)[0] + '/' + filename
 
     @staticmethod
     def search(path):
         try:
-            return File.query().filter( File.path == path ).one()
+            return File.query().filter(File.path == path).one()
         except NoResultFound:
             return None
 
     @staticmethod
     def save(ppath, filename, content, filetype='file/file',
-                        mimetype=None, permanent = True):
+             mimetype=None, permanent=True):
         parent = File.search(ppath)
         if not parent:
             raise RuntimeError('Parent path not found: %s' % ppath)
 
         if not mimetype:
-            mimetype = 'image/png' if filename.endswith('.png') else (
-                    'image/jpg' if filename.endswith('.jpg') else (
-                    'image/gif' if filename.endswith('.gif') else (
-                    'application/pdf' if filename.endswith('.pdf') else
-                    'application/octet-stream' )))
+            mimetype = {
+                '.png': 'image/png',
+                '.jpg': 'image/jpg',
+                '.gif': 'image/gif',
+                '.pdf': 'application/pdf',
+            }.get(pathlib.Path(filename.lower()).suffix, 'application/octet-stream')
+
+            mimetype_2 = 'image/png' if filename.endswith('.png') else (
+                       'image/jpg' if filename.endswith('.jpg') else (
+                           'image/gif' if filename.endswith('.gif') else (
+                               'application/pdf' if filename.endswith('.pdf') else
+                       'application/octet-stream' )))
 
         fullpath = ppath + filename if not ppath.endswith('/') else ppath + '/' + filename
 
-        file = File( path = fullpath, type = filetype, parent = parent,
-                    mimetype = mimetype, permanent = permanent, bindata = content )
+        file = File(path=fullpath, type=filetype, parent=parent,
+                    mimetype=mimetype, permanent=permanent, bindata=content)
 
         return file
 
-
+# EOF
