88743ea1e42c0535486a3bc9a592db3a40b3d5f9
anto_at_prodesk <anto@eijkman.go.id>
2021-07-28 07:07:43 +0700

    reformat to pep8 style

--- rhombus/lib/mgr.py
+++ rhombus/lib/mgr.py
@@ -1,15 +1,17 @@
 
-import sys, yaml, transaction
+import sys
+import yaml
+import transaction
 from rhombus.lib.utils import get_dbhandler, cerr, cout, cexit
 from rhombus.scripts import setup_settings, arg_parser
 
-## rbmgr.py
-##
-## this command manages datatype directly related to rhombus only
-## ie: userclass, user, group, enumkey
+# rbmgr.py
+#
+# this command manages datatype directly related to rhombus only
+# ie: userclass, user, group, enumkey
 
 
-def init_argparser( parser = None):
+def init_argparser(parser=None):
 
     if parser is None:
         p = arg_parser('rbmgr [rhombus]')
@@ -19,35 +21,34 @@ def init_argparser( parser = None):
     # commands
 
     p.add_argument('--initdb', action='store_true', default=False,
-        help = 'initialize database')
-
+                   help='initialize database')
 
     # import/export to YAML
 
     p.add_argument('--importuserclass', default=False, action='store_true',
-        help = 'import userclass from YAML file')
+                   help='import userclass from YAML file')
 
     p.add_argument('--exportuserclass', default=False, action='store_true',
-        help = 'export userclass to YAML file')
+                   help='export userclass to YAML file')
     # exported YAML file need to contain certain field for format recognizition
 
     p.add_argument('--importgroup', default=False,
-        help = 'import group from YAML file')
+                   help='import group from YAML file')
 
     p.add_argument('--exportgroup', default=False,
-        help = 'export group from YAML file')
+                   help='export group from YAML file')
 
     p.add_argument('--importenumkey', default=False,
-        help = 'import enumerated key from YAML file')
+                   help='import enumerated key from YAML file')
 
     p.add_argument('--exportenumkey', default=False,
-        help = 'export enumerated key to YAML file')
+                   help='export enumerated key to YAML file')
 
     p.add_argument('--listuserclass', default=False, action='store_true',
-        help = 'list user class(es)')
+                   help='list user class(es)')
 
     p.add_argument('--listuser', default=False, action='store_true',
-        help = 'list user(s) from a userclass')
+                   help='list user(s) from a userclass')
 
     p.add_argument('--listgroup', default=False, action='store_true',
                    help='list all groups')
@@ -66,11 +67,10 @@ def init_argparser( parser = None):
     # direct manipulation
 
     p.add_argument('--adduser', action='store_true', default=False,
-        help = 'add a new user')
+                   help='add a new user')
 
     p.add_argument('--setcred', action='store_true', default=False,
-        help = 'change user credential')
-
+                   help='change user credential')
 
     # specific options
 
@@ -100,33 +100,32 @@ def init_argparser( parser = None):
     return db_argparser(p)
 
 
-def db_argparser( p ):
+def db_argparser(p):
 
     # all db-related options here
 
     p.add_argument('-c', '--config', default=False,
-        help = 'config file (or use RHOMBUS_CONFIG environment)')
+                   help='config file (or use RHOMBUS_CONFIG environment)')
 
     p.add_argument('--commit', action='store_true', default=False,
-        help = 'commit the changes to database')
+                   help='commit the changes to database')
 
     p.add_argument('--rollback', action='store_true', default=False,
-        help = 'do no commit the changes and rollback the database')
+                   help='do no commit the changes and rollback the database')
 
     return p
 
 
-
 def main(args):
 
-    settings = setup_settings( args )
+    settings = setup_settings(args)
 
-    if any( (args.exportuserclass, args.exportgroup, args.exportenumkey) ):
-        do_rbmgr( args, settings )
+    if any((args.exportuserclass, args.exportgroup, args.exportenumkey)):
+        do_rbmgr(args, settings)
 
     elif not args.rollback and (args.commit or args.initdb):
         with transaction.manager:
-            do_rbmgr( args, settings )
+            do_rbmgr(args, settings)
             cerr('** COMMIT database **')
 
     else:
@@ -135,14 +134,13 @@ def main(args):
             keys = input('Do you want to continue? ')
             if keys.lower()[0] != 'y':
                 sys.exit(1)
-        do_rbmgr( args, settings )
+        do_rbmgr(args, settings)
 
 
-
-def do_rbmgr(args, settings, dbh = None):
+def do_rbmgr(args, settings, dbh=None):
 
     if not dbh:
-        dbh = get_dbhandler(settings, initial = args.initdb)
+        dbh = get_dbhandler(settings, initial=args.initdb)
 
     if args.initdb:
         do_initdb(args, dbh, settings)
@@ -192,14 +190,13 @@ def do_rbmgr(args, settings, dbh = None):
     return True
 
 
-
 def do_initdb(args, dbh, settings):
 
     print('do_initdb()')
 
-    dbh.initdb(create_table = (not args.no_create_table),
-            init_data = (not args.no_init_data),
-            rootpasswd = args.credential or None)
+    dbh.initdb(create_table=(not args.no_create_table),
+               init_data=(not args.no_init_data),
+               rootpasswd=args.credential or None)
     from rhombus.scripts.setup import populate_db
     userclass = yaml.load(open(args.initial_userclass)) if args.initial_userclass else None
     groups = yaml.load(open(args.initial_groups)) if args.initial_groups else None
@@ -213,7 +210,7 @@ def do_adduser(args, dbh, settings):
 
     if not args.userclass:
         cexit('ERR - please provide userclass')
-    domain = dbh.get_userclass( args.userclass )
+    domain = dbh.get_userclass(args.userclass)
     if not domain:
         cexit('ERR - userclass: %s does not exist!' % args.userclass)
 
@@ -222,7 +219,7 @@ def do_adduser(args, dbh, settings):
 
     if args.inquire:
         # get user detail remotely
-        args.lastname, args.firstname, args.email = domain.inquire_user( args.login )
+        args.lastname, args.firstname, args.email = domain.inquire_user(args.login)
         if not args.lastname:
             cexit('ERR - cannot inquire username: %s' % args.login)
         cerr('Obtaining: %s, %s [%s]' % (args.lastname, args.firstname, args.email))
@@ -244,10 +241,10 @@ def do_adduser(args, dbh, settings):
     else:
         groups = []
 
-    user = domain.add_user( login = args.login, email = args.email,
-                            lastname = args.lastname, firstname = args.firstname,
-                            primarygroup = args.primarygroup,
-                            groups = groups)
+    user = domain.add_user(login=args.login, email=args.email,
+                           lastname=args.lastname, firstname=args.firstname,
+                           primarygroup=args.primarygroup,
+                           groups=groups)
 
     cout('User %s added sucessfully.' % user.login)
 
@@ -290,14 +287,14 @@ def do_addenumkey(args, dbh, settings):
 
     ekey = dbh.add_ekey(key, key_group)
     cerr('I - enumkey: %s / %s has been added.' %
-        (ekey.key, ekey.group.key if ekey.group else '*'))
+         (ekey.key, ekey.group.key if ekey.group else '*'))
 
 
 def do_listenumkey(args, dbh, settings):
 
     cerr('List enumkey')
 
-    ekeys = dbh.list_ekeys(group = args.ekeygroup)
+    ekeys = dbh.list_ekeys(group=args.ekeygroup)
     for ek in ekeys:
         cout('%s' % ek.key)
 
@@ -311,15 +308,15 @@ def do_exportenumkey(args, dbh, settings):
     if args.exportenumkey:
         for ekey_code in args.exportenumkey.split(','):
             cerr('Getting [%s]' % ekey_code)
-            ekeys.append( dbh.get_ekey(ekey_code) )
+            ekeys.append(dbh.get_ekey(ekey_code))
     else:
         ekeys = dbh.list_ekeys()
 
     for ekey in ekeys:
-            ekey_list.append(ekey.as_dict())
+        ekey_list.append(ekey.as_dict())
 
     if args.outfile != '-':
-        open(args.outfile, 'w').write( yaml.dump( ekey_list) )
+        open(args.outfile, 'w').write(yaml.dump(ekey_list))
 
 
 def do_importenumkey(args, dbh, settings):
@@ -335,7 +332,7 @@ def do_importgroup(args, dbh, settings):
     cerr('Importing group')
     groups = yaml.load(open(args.importgroup))
 
-    from rhombus.models.user import UserClass, Group
+    from rhombus.models.user import Group
 
     Group.bulk_insert(groups, dbsession=dbh.session())
 
@@ -373,7 +370,7 @@ def do_exportuserclass(args, dbh, settings):
         userclasses = dbh.get_userclass()
 
     outfile = open(args.outfile, 'w')
-    dbh.UserClass.dump( outfile, userclasses )
+    dbh.UserClass.dump(outfile, userclasses)
 
 
 def do_rbdump(args, dbh, settings):
