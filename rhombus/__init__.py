6ad8c318c65bd14d666be3b0ed6a7c5ec8fe1652
anto @ aleph <trimarsanto@gmail.com>
2022-08-18 07:55:37 +0700

    split routes from __init__.py

--- rhombus/__init__.py
+++ rhombus/__init__.py
@@ -12,7 +12,7 @@ Please read the README.txt of this software.
 from pyramid.config import Configurator
 from pyramid.request import Request
 from pyramid.authentication import AuthTktAuthenticationPolicy
-from pyramid.session import SignedCookieSessionFactory
+
 from pyramid.events import BeforeRender
 
 import dogpile.cache
@@ -21,159 +21,10 @@ import dogpile.cache.util
 from rhombus import configkeys as ck
 from rhombus.lib.utils import cout, cerr, get_dbhandler, random_string, dbhandler_userid_func, set_func_userid
 from rhombus.lib import helpers as h
-from rhombus.lib import exceptions as exc
-from rhombus.views import generics
-
+from rhombus.routes import includeme
 from rhombus.scripts import run
 
 _TITLE_ = ''
-
-
-def includeme(config):
-
-    cerr('rhombus configuration with prefix: %s' % config.route_prefix)
-
-    # configure exception handler view
-    config.add_exception_view(generics.autherror_page, exc.AuthError)
-
-    config.include('pyramid_mako')
-    config.add_static_view(name='rhombus_static', path="rhombus:static/")
-
-    session_factory = SignedCookieSessionFactory(random_string(64))
-    config.set_session_factory(session_factory)
-
-    # configure RbRequest
-
-    # configure exception views if debugtoolbar is not enabled
-    settings = config.get_settings()
-    if 'debugtoolbar.includes' not in settings:
-        cerr('WARN: setting up in full deployment configuration!')
-        config.add_view('rhombus.views.generics.usererror_page', context=RuntimeError)
-        config.add_view('rhombus.views.generics.syserror_page', context=Exception)
-
-    # configure routes & views
-
-    if config.route_prefix:
-        config.add_route('rhombus.dashboard', '/')
-    else:
-        config.add_route('rhombus.dashboard', '/dashboard')
-    config.add_view('rhombus.views.dashboard.index', route_name='rhombus.dashboard')
-
-    add_route_view(
-        config, 'rhombus.views.group', 'rhombus.group',
-        '/group',
-        '/group/@@action',
-        '/group/@@user_action',
-        '/group/@@role_action',
-        ('/group/@@lookup', 'lookup', 'json'),
-        '/group/{id}@@edit',
-        '/group/{id}@@save',
-        ('/group/{id}', 'view'),
-    )
-
-    add_route_view(
-        config, 'rhombus.views.ek', 'rhombus.ek',
-        '/ek',
-        '/ek/@@action',
-        ('/ek/@@lookup', 'lookup', 'json'),
-        '/ek/{id}@@edit',
-        '/ek/{id}@@save',
-        ('/ek/{id}', 'view'),
-    )
-
-    add_route_view(
-        config, 'rhombus.views.userclass', 'rhombus.userclass',
-        '/userclass',
-        '/userclass/@@action',
-        '/userclass/{id}@@edit',
-        ('/userclass/{id}', 'view'),
-    )
-
-    add_route_view(
-        config, 'rhombus.views.user', 'rhombus.user',
-        '/user',
-        '/user/@@action',
-        '/user/@@passwd',
-        ('/user/@@lookup', 'lookup', 'json'),
-        '/user/{id}@@edit',
-        # '/user/{id}@@passwd',
-        ('/user/{id}', 'view'),
-    )
-
-    add_route_view(
-        config, 'rhombus.views.gallery', 'rhombus.gallery',
-        '/gallery',
-    )
-
-    # for overriding assets
-    override_assets(
-        config, settings,
-        [
-            (ck.override_loginpage, 'rhombus:templates/login.mako'),
-        ]
-    )
-
-    if ck.override_assets in settings:
-        assets = settings['override.assets']
-        for asset in assets.split('\n'):
-            if not asset:
-                continue
-            asset_pair = [a.strip() for a in asset.split('>')]
-            print('overriding: %s >> %s' % (asset_pair[0], asset_pair[1]))
-            config.override_asset(asset_pair[0], asset_pair[1])
-
-
-def add_route_view(config, view_module, prefix_name, *routelist):
-    for route_args in routelist:
-        renderer = None
-        if type(route_args) == str:
-            url = route_args
-            if '@@' in route_args:
-                view_name = route_args.split('@@')[-1]
-                route_name = '%s-%s' % (prefix_name, view_name)
-            else:
-                view_name = 'index'
-                route_name = prefix_name
-        else:
-            url = route_args[0]
-            view_name = route_args[1]
-            route_name = '%s-%s' % (prefix_name, view_name)
-            if len(route_args) > 2:
-                renderer = route_args[2]
-
-        config.add_route(route_name, url)
-        config.add_view(
-            '%s.%s' % (view_module, view_name),
-            route_name=route_name,
-            renderer=renderer
-        )
-
-
-def add_route_view_class(config, view_class, prefix_name, *routelist):
-    for route_args in routelist:
-        renderer = None
-        if type(route_args) == str:
-            url = route_args
-            if '@@' in route_args:
-                view_name = route_args.split('@@')[-1]
-                route_name = '%s-%s' % (prefix_name, view_name)
-            else:
-                view_name = 'index'
-                route_name = prefix_name
-        else:
-            url = route_args[0]
-            view_name = route_args[1]
-            route_name = '%s-%s' % (prefix_name, view_name)
-            if len(route_args) > 2:
-                renderer = route_args[2]
-
-        config.add_route(route_name, url)
-        config.add_view(
-            view_class,
-            attr=view_name,
-            route_name=route_name,
-            renderer=renderer
-        )
 
 
 def init_app(global_config, settings, prefix=None, dbhandler_factory=get_dbhandler,
@@ -558,25 +409,6 @@ def confirm_token(url, token):
     if not r.ok:
         raise RuntimeError("ERROR: principal authenticator failed to respond properly!")
     return r.json()
-
-
-def override_assets(config, settings, asset_list):
-    """ asset_list: [ (cfg, asset, def_overrider), ...]
-                eg. [ ('override.base', 'rhombus:templates/base.mako',
-                        'rhombus:templates/my_base.mako') ]
-                with override.base as optional tag in config file, so that asset can also
-                be override using config file
-    """
-
-    for cfg, asset in asset_list:
-        override = settings.get(cfg, asset)
-        if override == asset:
-            continue
-        print("Overriding asset [%s] with [%s]" % (asset, override))
-        config.override_asset(
-            to_override=asset,
-            override_with=override
-        )
 
 
 def add_global(event):
