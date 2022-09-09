d62a6bfd5284a8c86cb6958a0df5c119aa273703
anto @ aleph <trimarsanto@gmail.com>
2022-09-09 18:06:56 +0700

    implement modal_error(); reformat to pep8 style

--- rhombus/lib/modals.py
+++ rhombus/lib/modals.py
@@ -1,27 +1,34 @@
 
-from rhombus.lib.tags import *
+from rhombus.lib.tags import literal
 
 from pyramid.renderers import render
 
 
 def popup(title, content, footer=None, request=None):
+    return literal(render("rhombus:templates/generics/popup.mako", {
+        'title': title,
+        'content': content,
+        'buttons': footer if footer else '',
+    }, request=request))
 
-    return literal( render("rhombus:templates/generics/popup.mako",
-            {   'title': title,
-                'content': content,
-                'buttons': footer if footer else '',
-            }, request = request ))
 
 def modal_delete(title, content, request, value='delete/confirm'):
     return popup(
-        title = title,
-        content = content,
-        footer = literal(
+        title=title,
+        content=content,
+        footer=literal(
             '<button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>'
             '<button class="btn btn-danger" type="submit" name="_method" value="%s">'
             'Confirm Delete</button>' % value),
-        request = request
+        request=request
     )
 
-def modal_error():
-    raise NotImplementedError()
+
+def modal_error(title='Error', content='', request=None):
+    return popup(
+        title=title,
+        content=content,
+        request=request
+    )
+
+# EOF
