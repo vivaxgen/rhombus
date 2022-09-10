957343ef2693d400fe871ff4ba42d61ad6a543ef
anto @ aleph <trimarsanto@gmail.com>
2022-09-10 18:57:09 +0700

    reformat to pep8 style; fix typos

--- rhombus/lib/fsoverlay.py
+++ rhombus/lib/fsoverlay.py
@@ -8,7 +8,8 @@ from rhombus.lib.utils import random_string
 
 from pyramid.path import AssetResolver
 
-import os, yaml
+import os
+import yaml
 
 
 # mount points for the fslayer
@@ -17,30 +18,32 @@ _MOUNTS = []
 
 def fsomount(virtual_path, absolute_path):
     _MOUNTS.append(
-        (os.path.normpath(virtual_path), os.path.normpath(absolute_path)) )
+        (os.path.normpath(virtual_path), os.path.normpath(absolute_path))
+    )
 
 
-def mkranddir( parent_directory, userid ):
-    d = FileOverlay(abspath = '%s/%s/' % (parent_directory, random_string(12)),
-    	type = 'dir')
+def mkranddir(parent_directory, userid):
+    d = FileOverlay(abspath='%s/%s/' % (parent_directory, random_string(12)),
+                    type='dir')
     d.add_permission(userid)
     d.create_directory()
     return d
 
 
 def get_absmount(vpath):
-	""" return mount_point """
-	for virpath, abspath in _MOUNTS:
-		if vpath.startswith( virpath ):
-			return (virpath, abspath)
-	return None
+    """ return mount_point """
+    for virpath, abspath in _MOUNTS:
+        if vpath.startswith(virpath):
+            return (virpath, abspath)
+    return None
+
 
 def get_virtmount(apath):
-	""" return mount point """
-	for virpath, abspath in _MOUNTS:
-		if apath.startswith( abspath ):
-			return (virpath, abspath)
-	return None
+    """ return mount point """
+    for virpath, abspath in _MOUNTS:
+        if apath.startswith(abspath):
+            return (virpath, abspath)
+    return None
 
 
 def get_abspath(vpath, mp=None):
@@ -55,7 +58,7 @@ def get_virtpath(apath, mp=None):
     """ return a virtual path from an absolute path """
     mp = mp or get_virtmount(apath)
     if mp:
-    	return mp[0] + apath[len(mp[1]):]
+        return mp[0] + apath[len(mp[1]):]
     return None
 
 
@@ -63,28 +66,28 @@ def get_urlpath(abspath):
     return '/fso' + get_virtpath(abspath)
 
 
-def return_as_response( rpath = None, vpath = None):
+def return_as_response(rpath=None, vpath=None):
     pass
 
 ##
-## .rhoacl is a YAML file, which provides authorization on the directory
-## (more or less similar to apache .htaccess scheme)
+# .rhoacl is a YAML file, which provides authorization on the directory
+# (more or less similar to apache .htaccess scheme)
 ##
 
 
 class FileOverlay(object):
 
     def __init__(self, virtpath=None, abspath=None, type='file',
-                        mount_point=None):
+                 mount_point=None):
         """ mount_point: (virtual_path, absolute_path)
         """
         self.mount_point = None
         if mount_point:
             ar = AssetResolver()
             self.mount_point = (mount_point[0],
-                            ar.resolve(mount_point[1]).abspath() + '/')
+                                ar.resolve(mount_point[1]).abspath() + '/')
         if (virtpath and abspath):
-        	raise RuntimeError('ERR - need only virtpath nor abspath')
+            raise RuntimeError('ERR - need only virtpath nor abspath')
         if virtpath:
             self.virtpath = os.path.normpath(virtpath)
             if mount_point is None:
@@ -96,45 +99,42 @@ class FileOverlay(object):
                 self.mount_point = get_virtmount(self.abspath)
             self.virtpath = get_virtpath(self.abspath, self.mount_point)
         else:
-        	raise RuntimeERror('ERR - need either virtpath or abspath')
+            raise RuntimeError('ERR - need either virtpath or abspath')
         self.parent = None
         self._meta = None
         self.type = type
         self.mimetype = None
 
-
     def create_directory(self):
         if self.type == 'dir':
-            os.mkdir( self.abspath )
+            os.mkdir(self.abspath)
         else:
-        	raise RuntimeError(
-        		'ERR - can only create directory from directory type'
-        	)
+            raise RuntimeError(
+                'ERR - can only create directory from directory type'
+            )
         with open(self.abspath + '/.rhoacl', 'w') as metafile:
-        	yaml.dump(self.meta, metafile)
+            yaml.dump(self.meta, metafile)
         # create .rhoauth as well
-
 
     @staticmethod
     def save(parentpath, filename, content, filetype='file/file',
-                    mimetype=None, permanent = False):
-        parent = FileOverlay.open( parentpath )
+             mimetype=None, permanent=False):
+        parent = FileOverlay.open(parentpath)
         if not parent:
-            raise RuntimeError('Parent path not found: %s' % ppath)
-
+            raise RuntimeError('Parent path not found: %s' % parentpath)
 
     @staticmethod
     def openfile(virtpath, mode='r', mount_point=None):
         """ mount_point: (virtual_path, absolute_path )
         """
         if mode == 'r':
-            fso_obj = FileOverlay( virtpath = virtpath,
-                        mount_point = mount_point )
+            fso_obj = FileOverlay(virtpath=virtpath,
+                                  mount_point=mount_point)
             return fso_obj
 
     @staticmethod
     def opendir(virtpath, mode='r'):
-    	pass
+        pass
 
     @property
     def urlpath(self):
@@ -160,26 +160,22 @@ class FileOverlay(object):
             return self._meta
         raise RuntimeError('ERR - unknown FileOverlay type')
 
-
     def add_permission(self, user):
-    	if 'users' in self.meta:
-    		self.meta['users'].append( user )
-    	else:
-    		self.meta['users'] = [ user ]
-
+        if 'users' in self.meta:
+            self.meta['users'].append(user)
+        else:
+            self.meta['users'] = [user]
 
     def check_user_permission(self, user, mode=None):
-    	if 'users' in self.meta and user in self.meta['users']:
-    		return True
-    	return False
+        if 'users' in self.meta and user in self.meta['users']:
+            return True
+        return False
 
     def check_groups_permission(self, groups, mode=None):
         pass
 
-
     def read(self):
         pass
-
 
     def write(self, data):
         pass
@@ -187,3 +183,5 @@ class FileOverlay(object):
     @staticmethod
     def get_urlpath(abspath):
         return get_urlpath(abspath)
+
+# EOF
