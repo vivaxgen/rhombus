6b13a6c2fc1093126a86cf4efeaf8f13fe7ba3ca
anto @ aleph <trimarsanto@gmail.com>
2023-01-24 13:22:27 +0700

    reformat to pep8 style

--- rhombus/views/fso.py
+++ rhombus/views/fso.py
@@ -1,8 +1,7 @@
 
 from pyramid.response import FileResponse
 
-from rhombus.views import *
-from rhombus.views import roles
+from rhombus.views import roles, PUBLIC, error_page
 from rhombus.lib.fsoverlay import FileOverlay
 from rhombus.lib import rst
 
@@ -12,9 +11,9 @@ import shutil, os, re
 
 
 def get_file_size(file):
-    file.seek(0, 2) # Seek to the end of the file
-    size = file.tell() # Get the position of EOF
-    file.seek(0) # Reset the file position to the beginning
+    file.seek(0, 2)     # Seek to the end of the file
+    size = file.tell()  # Get the position of EOF
+    file.seek(0)        # Reset the file position to the beginning
     return size
 
 
@@ -46,7 +45,7 @@ def save_file(destpath, filestorage, request):
         mo = re.match(r'\D+\W+(\d+)-(\d+)\/(\d+)', content_range)
         values = mo.groups()
         if len(values) == 3:
-            begin, end, total = tuple( int(x) for x in values )
+            begin, end, total = tuple(int(x) for x in values)
         else:
             raise RuntimeError('ERR - unparseable Content-Range header!')
 
@@ -67,14 +66,14 @@ def save_file(destpath, filestorage, request):
                 shutil.copyfileobj(fileobj, f)
                 size = get_file_size(f)
 
-        if not end == size-1:
+        if not end == size - 1:
             raise RuntimeError('ERR - writing to file %s' % destpath)
 
         return (size, total)
 
 
 def serve_file(path, user=None, mount_point=None, formatter=None,
-                    virtual_indexer=False):
+               virtual_indexer=False):
     """ mount_point: (virtual_path, absolute_path)
         formatter: a_func( absolute_file_path), returning a Response
         virtual_indexer: a_func( absolute_file_path ) if absolute_file_path is
@@ -88,25 +87,25 @@ def serve_file(path, user=None, mount_point=None, formatter=None,
     abspath = fso_file.abspath
     if os.path.isdir(abspath):
         if virtual_indexer:
-            return virtual_indexer( abspath )
+            return virtual_indexer(abspath)
         raise RuntimeError('ERR - virtual index not allowed!')
 
     if formatter:
-        return formatter( abspath )
+        return formatter(abspath)
 
-    return FileResponse( abspath )
+    return FileResponse(abspath)
 
 
 def render_rst(text, format='html'):
 
-    parts = docutils.core.publish_parts( text, writer_name=format,
-        settings_overrides={'initial_header_level': 2} )
+    parts = docutils.core.publish_parts(text, writer_name=format,
+                                        settings_overrides={'initial_header_level': 2})
     if format == 'html':
         return parts['html_body']
     return None
 
 
-@roles( PUBLIC )
+@roles(PUBLIC)
 def index(request):
     """ send the content of file to browser
     """
@@ -119,24 +118,26 @@ def index(request):
     if not fso_file.check_user_permission(request.identity.login, 'r'):
         return error_page('ERR - authorization error, permission denied.')
 
-    return FileResponse( fso_file.abspath )
+    return FileResponse(fso_file.abspath)
 
 
-@roles( PUBLIC )
+@roles(PUBLIC)
 def view(request):
     pass
 
 
-@roles( PUBLIC )
+@roles(PUBLIC)
 def edit(request):
     pass
 
 
-@roles( PUBLIC )
+@roles(PUBLIC)
 def save(request):
     pass
 
 
-@roles( PUBLIC )
+@roles(PUBLIC)
 def action(request):
     pass
+
+# EOF
